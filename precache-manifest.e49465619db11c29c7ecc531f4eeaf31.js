b54b8160d6a4e1261c95f9e3670051a531f1a2cc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-21 18:21:21 +0000

    Deploy n1ghtdev/scroll-editor to github.com/n1ghtdev/scroll-editor.git:gh-pages

---
+++ precache-manifest.e49465619db11c29c7ecc531f4eeaf31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b682b52a99ed71984f38a445f057b72b",
+    "url": "/scroll-editor/index.html"
+  },
+  {
+    "revision": "e747357c4cbbff3246ac",
+    "url": "/scroll-editor/static/css/main.14b35ed7.chunk.css"
+  },
+  {
+    "revision": "b69a6fb39daa63ec612a",
+    "url": "/scroll-editor/static/js/2.72b662b1.chunk.js"
+  },
+  {
+    "revision": "f1980d509ecacd7d6a769876a20c389f",
+    "url": "/scroll-editor/static/js/2.72b662b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e747357c4cbbff3246ac",
+    "url": "/scroll-editor/static/js/main.c4f3a53e.chunk.js"
+  },
+  {
+    "revision": "f79fc9658dc9e3df2ee4",
+    "url": "/scroll-editor/static/js/runtime-main.488b76b7.js"
+  }
+]);
